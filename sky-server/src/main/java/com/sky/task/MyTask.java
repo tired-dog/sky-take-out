4a9777708206c75ac8f1d1576040615c1d93d3e5
tired-dog <134507325+tired-dog@users.noreply.github.com>
2024-11-14 23:35:29 +0800

    订单状态定时处理

---
+++ sky-server/src/main/java/com/sky/task/MyTask.java
@@ -0,0 +1,14 @@
+package com.sky.task;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * 自定义定时任务类
+ */
+@Component
+@Slf4j
+public class MyTask {
+
+
+}
